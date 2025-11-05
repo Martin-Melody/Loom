620afbadd6dd1d1039d26d5b4871fdd180e55d04
Martin Melody <martinmelody.work@gmail.com>
2025-11-05 14:40:34 +0000

    Updating Command to include Id and Shortcut properties

--- src/Core/Entities/Command.cs
+++ src/Core/Entities/Command.cs
@@ -2,15 +2,25 @@ namespace Loom.Core.Entities;
 
 public class Command
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
+    public string? Shortcut { get; set; }
     public Func<Task> Action { get; set; }
 
-    public Command(string name, string category, Func<Task> action)
+    public Command(
+        Guid id,
+        string name,
+        string category,
+        Func<Task> action,
+        string? shortcut = null
+    )
     {
+        Id = id;
         Name = name;
         Category = category;
         Action = action;
+        Shortcut = shortcut;
     }
 
     public override string ToString() => $"{Category}: {Name}";
