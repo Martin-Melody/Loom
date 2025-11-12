171ac4052d37c47b1076c809402528a83a7b6056
Martin Melody <martinmelody.work@gmail.com>
2025-11-12 17:40:41 +0000

    adding sidebar state to app config and using new enums

--- src/Core/Entities/AppConfig.cs
+++ src/Core/Entities/AppConfig.cs
@@ -1,6 +1,9 @@
 namespace Loom.Core.Entities;
 
+using Loom.Core.Entities.Enums;
+
 public class AppConfig
 {
-    public string LastOpenView { get; set; } = "Dashboard";
+    public ViewType LastOpenView { get; set; } = ViewType.Dashboard;
+    public SidebarState SidebarState { get; set; } = SidebarState.Closed;
 }
