15b36089e616975ffb72f926bf108dfff87d1da8
Martin Melody <martinmelody.work@gmail.com>
2025-12-03 17:16:42 +0000

    Add Command functionality for showing the connection settings screen

--- src/UI.Terminal/Commands/GlobalCommandDefinitions.cs
+++ src/UI.Terminal/Commands/GlobalCommandDefinitions.cs
@@ -61,6 +61,14 @@ public static class GlobalCommandDefinitions
                 isGlobalShortcut: true
             ),
             new CommandDefinition(
+                CommandIds.Settings.ShowConnectionSettings,
+                "Open Connection Settings",
+                "Settings",
+                app.ShowConnectionSettings,
+                shortcut: "Ctrl+4",
+                isGlobalShortcut: true
+            ),
+            new CommandDefinition(
                 CommandIds.Navigation.ToggleSidebar,
                 "Toggle Sidebar",
                 "Navigation",
